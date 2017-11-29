4e75b4df7e68d551d9408f74d911db20b85010f8
alasch2 <ala.schneider@sealights.io>
2017-11-29 15:33:10 +0200

    Application skeleton

---
+++ src/main/java/io/sealights/casestudy/disneystudio/ensemble/EnsembleApplication.java
@@ -0,0 +1,12 @@
+package io.sealights.casestudy.disneystudio.ensemble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsembleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsembleApplication.class, args);
+	}
+}
---
+++ src/test/java/io/sealights/casestudy/disneystudio/ensemble/EnsembleApplicationTests.java
@@ -0,0 +1,16 @@
+package io.sealights.casestudy.disneystudio.ensemble;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EnsembleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
